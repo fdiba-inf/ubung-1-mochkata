e989b8f0d4a23bb617309c146488d223786f9024
mochkata <>
2021-10-13 15:20:43 +0000

    AzIs

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -5,7 +5,7 @@ public class Tree {
      public static void main(String[] args) { 
             System.out.println("    *");
             System.out.println("   ***");
-            System.out.println("  ***** ");
+            System.out.println("  *****");
             System.out.println(" *******");
             System.out.println("*********");
             System.out.println("   ***");
